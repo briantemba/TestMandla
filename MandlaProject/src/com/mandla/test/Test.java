57c7c8e4d23c8e9f04f1cf477cfb791fea24a7d5
mandla <gilbeemhh@yahoo.com>
2016-01-13 20:13:33 +0200

    First commit

---
+++ MandlaProject/src/com/mandla/test/Test.java
@@ -0,0 +1,11 @@
+package com.mandla.test;
+
+public class Test {
+
+  public static void main(String[] args) {
+    System.out.println("Done");
+    System.out.println("This is a test");
+
+  }
+
+}
